e95b9f80154c82b6d81044bb086ac6ca7c796ed6
Shen Li <shenli3@tareka02.cs.uiuc.edu>
2012-06-28 17:07:18 -0500

    ...

---
+++ scripts/install_mysql.sh
@@ -0,0 +1,4 @@
+cp /home/tarek/shenli3/software/src/mysql-5.5.19-linux2.6-x86_64.tar.gz /scratch/shenli3/software/
+tar -zxvf /scratch/shenli3/software/mysql-5.5.19-linux2.6-x86_64.tar.gz -C /scratch/shenli3/software/
+cd /scratch/shenli3/software/mysql-5.5.19-linux2.6-x86_64
+./scripts/mysql_install_db --user=shenli3
---
+++ scripts/install_mysql_32.sh
@@ -0,0 +1,4 @@
+cp /home/tarek/shenli3/software/src/mysql-5.5.25-linux2.6-i686.tar.gz /scratch/shenli3/software/
+tar -zxvf /scratch/shenli3/software/mysql-5.5.25-linux2.6-i686.tar.gz -C /scratch/shenli3/software/
+cd /scratch/shenli3/software/mysql-5.5.25-linux2.6-i686
+./scripts/mysql_install_db --user=shenli3
